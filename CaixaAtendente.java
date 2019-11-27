dd234515cef836e2ed06ffc978976859ed6e6cd8
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-11-27 10:16:28 -0300

    Métodos getters e setters de cada classe

--- CaixaAtendente.java
+++ CaixaAtendente.java
@@ -1,3 +1,40 @@
-public class CaixaAtendente extends Pessoa {
+import java.util.Date;
 
+public class CaixaAtendente extends Pessoa {
+    private Date horarioEntrada;
+    private Date horarioSaida;
+    private Caixa caixa;
+
+    public CaixaAtendente() {
+    }
+
+    public CaixaAtendente(Date horarioEntrada, Date horarioSaida, Caixa caixa) {
+        this.horarioEntrada = horarioEntrada;
+        this.horarioSaida = horarioSaida;
+        this.caixa = caixa;
+    }
+
+    public Date getHorarioEntrada() {
+        return this.horarioEntrada;
+    }
+
+    public void setHorarioEntrada(Date horarioEntrada) {
+        this.horarioEntrada = horarioEntrada;
+    }
+
+    public Date getHorarioSaida() {
+        return this.horarioSaida;
+    }
+
+    public void setHorarioSaida(Date horarioSaida) {
+        this.horarioSaida = horarioSaida;
+    }
+
+    public Caixa getCaixa() {
+        return this.caixa;
+    }
+
+    public void setCaixa(Caixa caixa) {
+        this.caixa = caixa;
+    }
 }
