6cc64ee302fa9aefa1d26f1385a0110d598dda71
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-11-27 10:15:11 -0300

    Implementando a classe Caixa

---
+++ Caixa.java
@@ -0,0 +1,43 @@
+public class Caixa {
+    private int numero;
+    private Boolean preferencia;
+    private Boolean pequenasCompras;
+
+    public Caixa(int numero, Boolean preferencia, Boolean pequenasCompras) {
+        this.numero = numero;
+        this.preferencia = preferencia;
+        this.pequenasCompras = pequenasCompras;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Boolean isPreferencia() {
+        return this.preferencia;
+    }
+
+    public Boolean getPreferencia() {
+        return this.preferencia;
+    }
+
+    public void setPreferencia(Boolean preferencia) {
+        this.preferencia = preferencia;
+    }
+
+    public Boolean isPequenasCompras() {
+        return this.pequenasCompras;
+    }
+
+    public Boolean getPequenasCompras() {
+        return this.pequenasCompras;
+    }
+
+    public void setPequenasCompras(Boolean pequenasCompras) {
+        this.pequenasCompras = pequenasCompras;
+    }
+}
