e1ccbf9ccc56b719a646a34f4b015c6d066c505b
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-11-30 00:37:15 -0300

    Função create() no repositório de Caixa

--- CaixaRepositorio.java
+++ CaixaRepositorio.java
@@ -3,4 +3,8 @@ import java.util.ArrayList;
 public class CaixaRepositorio {
     private ArrayList<Caixa> caixas = new ArrayList<Caixa>();
 
+    public Caixa create(Caixa caixa) {
+        this.caixas.add(caixa);
+        return caixa;
+    }
 }
