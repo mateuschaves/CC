b0c9b58dc5dcbc50cccd392ff3d64753291b2d18
lucaszamorano <57903939+lucaszamorano@users.noreply.github.com>
2019-12-03 19:45:18 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,43 @@
+public class Produto {
+    private String nome;
+    private String marca;
+    private double valor;
+    private String distribuidor;
+
+    public Produto(String nome, String marca, double valor, String distribuidor) {
+        this.nome = nome;
+        this.marca = marca;
+        this.valor = valor;
+        this.distribuidor = distribuidor;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void getDistribuidor(String distribuidor) {
+        return this.distribuidor;
+    }
+
+public void setDistribuidor(String distribuidor) {
+        this.distribuidor = distribuidor;
+    }
