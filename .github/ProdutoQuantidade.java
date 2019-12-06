69579c4c1122768a7931a80d7df01eb27547bcbf
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-12-06 16:40:27 -0300

    Enhancement resoled #12

---
+++ .github/ProdutoQuantidade.java
@@ -0,0 +1,29 @@
+public class ProdutoQuantidade {
+    private Produto produto;
+    private int quantidade;
+
+    public ProdutoQuantidade(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public ProdutoQuantidade() {
+    }
+
+    public Produto getProduto() {
+        return this.produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
