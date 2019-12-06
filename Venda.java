82bc280806403ff8f060b17185479674309eaa48
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-12-06 16:46:01 -0300

    Classe de venda

---
+++ Venda.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Venda {
+    private Cliente cliente;
+    private ArrayList<ProdutoQuantidade> produtos = new ArrayList<ProdutoQuantidade>();
+    private Date dataCompra;
+    private float precoTotal;
+
+    public Venda() {
+    }
+
+    public Venda(Cliente cliente, ArrayList<ProdutoQuantidade> produtos, Date dataCompra, float precoTotal) {
+        this.cliente = cliente;
+        this.produtos = produtos;
+        this.dataCompra = dataCompra;
+        this.precoTotal = precoTotal;
+    }
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<ProdutoQuantidade> getProdutos() {
+        return this.produtos;
+    }
+
+    public void setProdutos(ArrayList<ProdutoQuantidade> produtos) {
+        this.produtos = produtos;
+    }
+
+    public Date getDataCompra() {
+        return this.dataCompra;
+    }
+
+    public void setDataCompra(Date dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public float getPrecoTotal() {
+        return this.precoTotal;
+    }
+
+    public void setPrecoTotal(float precoTotal) {
+        this.precoTotal = precoTotal;
+    }
+}
