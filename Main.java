811dc0cecf35f1734b95e87136c87727bd989241
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-12-06 16:41:41 -0300

    Comentando a criação de produto

--- Main.java
+++ Main.java
@@ -1,38 +1,34 @@
 import java.util.Calendar;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Calendar horaEntrada = Calendar.getInstance();
-        horaEntrada.set(Calendar.HOUR, 8);
+    Calendar horaEntrada = Calendar.getInstance();
+    horaEntrada.set(Calendar.HOUR, 8);
 
-        Calendar horaSaida = Calendar.getInstance();
-        horaSaida.set(Calendar.HOUR, 10);
+    Calendar horaSaida = Calendar.getInstance();
+    horaSaida.set(Calendar.HOUR, 10);
 
-        Caixa caixa = new Caixa(1, false, false);
-        CaixaAtendente caixaAtendente = new CaixaAtendente("Mateus Henrique", "115.058.024-07", caixa, horaEntrada,
-                horaSaida);
+    Caixa caixa = new Caixa(1, false, false);
+    CaixaAtendente caixaAtendente = new CaixaAtendente("Mateus Henrique", "115.058.024-07", caixa, horaEntrada,
+        horaSaida);
 
-        CaixaRepositorio repo = new CaixaRepositorio();
-        repo.criar(caixa);
+    CaixaRepositorio repo = new CaixaRepositorio();
+    repo.criar(caixa);
 
-        System.out.println("Nome: " + caixaAtendente.getNome());
-        System.out.println("Horario de entrada: " + caixaAtendente.getHorarioEntrada());
-        System.out.println("Horario de saida: " + caixaAtendente.getHorarioSaida());
+    System.out.println("Nome: " + caixaAtendente.getNome());
+    System.out.println("Horario de entrada: " + caixaAtendente.getHorarioEntrada());
+    System.out.println("Horario de saida: " + caixaAtendente.getHorarioSaida());
 
-        Produto produto = new Produto(1,"ninho","nestle",48, "vale", 15,7);
-          System.out.println("ID: " + produto.getId());
-        System.out.println("Nome: " + produto.getNome());
-        System.out.println("Marca: " + produto.getMarca());
-        System.out.println("Preco: " + produto.getValor());
-        System.out.println("distribuidor: " + produto.getDistribuidor());
-        System.out.println("Estoque: " + produto.getEstoque());
-        System.out.println("vendidos: " + produto.getCompra());
-        
-        
-        
-        
+    // Produto produto = new Produto(1,"ninho","nestle",48, "vale", 15,7);
+    // System.out.println("ID: " + produto.getId());
+    // System.out.println("Nome: " + produto.getNome());
+    // System.out.println("Marca: " + produto.getMarca());
+    // System.out.println("Preco: " + produto.getValor());
+    // System.out.println("distribuidor: " + produto.getDistribuidor());
+    // System.out.println("Estoque: " + produto.getEstoque());
+    // System.out.println("vendidos: " + produto.getCompra());
 
-    }
+  }
 
 }
